07155573ca9fee0148a7e4e79d84df36fd95e0f0
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-01-16 15:29:53 +0100

    main.ts, test.ts frissítése

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ namespace obMaths{
         if(x == 0) return 0
         if(x == 1) return 0
         if(x == 2) return 1
+        if(x % 2 == 0) return 0
         let numsqrt = Math.sqrt(x);
         for (let i = 3; i <= numsqrt; i+=2) {
             if (x % i == 0) return 0
---
+++ test.ts
@@ -0,0 +1,1 @@
+basic.showNumber(obMaths.isPrime(23))
