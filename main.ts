9e88f89fe496b2e119cd4de3d1328d8b81fe2ea7
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-01-16 14:38:07 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 //% color=#008060 weight=50 icon="\uf12b" block="obMaths"
 namespace obMaths{
-    //%blockId = obmathsxor
+    //%blockId = obMaths_xor
     //%block = "xor $a $b"
     export function xor(a: number, b: number): number{
         return a^b;
     }
-    //%blockId = obmathsprime
+    //%blockId = obMaths_prime
     //%block = "prime $x"
     export function prime(x: number): boolean{
         if(x == 0) return false
