74cf395d56e16cce0278758c1be590087a8eb66b
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-01-16 14:32:23 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% color=#008060 weight=50 icon="\uf12b" block="obMaths"
 namespace obMaths{
-    //%blockId = obmathsxor
+    //%blockId = obMaths_xor
     //%block = "xor $a $b"
     export function xor(a: number, b: number): number{
         return a^b;
