572f22d7a607545196d25a409f00a320aa69ab6e
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-01-16 14:47:04 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,25 +1,7 @@
-
 namespace obMaths{
     //%blockId = "obMaths_xor"
     //%block = "xor $a $b"
     export function xor(a: number, b: number): number{
         return a^b;
     }
-    //%blockId = "obMaths_or"
-    //%block = "xor $a $b"
-    export function or(a: number, b: number): number{
-        return a|b;
-    }
-//    //%blockId = "obMaths_prime"
-//    //%block = "prime $x"
-/*    export function prime(x: number): number{
-/*        if(x == 0) return false
-        if(x == 1) return false
-        if(x == 2) return true
-        let numsqrt = Math.sqrt(x);
-        for (let i = 3; i <= numsqrt; i+=2) {
-            if (x % i == 0) return false
-        }
-        return 1
-    }*/
 }
