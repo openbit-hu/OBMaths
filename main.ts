e3f268697e29e1dfd335aa1ed7aecd68c810e92f
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-01-16 15:16:34 +0100

    main.ts, test.ts frissítése

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
 //% color=#008060 weight=100 icon="\uf12b" block="obMaths";
 namespace obMaths{
-    //% blockId=obMaths_bor block="bor $a $b"
-    export function bor(a: number, b: number): number{
-        return a | b;
-    }
-
     //% blockId=obMaths_xor block="xor $a $b"
     export function xor(a: number, b: number): number{
         return a^b;
     }
-    
+    //% blockId=obMaths_prime block="prime $x"
+    export function prime(x: number): number{
+        if(x == 0) return 0
+        if(x == 1) return 0
+        if(x == 2) return 0
+        let numsqrt = Math.sqrt(x);
+        for (let i = 3; i <= numsqrt; i+=2) {
+            if (x % i == 0) 0
+        }
+        return 1
+    }
+
 }
--- test.ts
+++
@@ -1,5 +0,0 @@
-if(obMaths.bor(9,1) == 1){
-    basic.showString("Prím");
-} else{
-    basic.showString("Nem prím");
-}
