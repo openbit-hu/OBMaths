e4bb383c845b5dad56365e3054b73d82b1fecd5f
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-01-16 16:56:17 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -4,17 +4,28 @@ namespace obMaths{
     export function xor(a: number, b: number): number{
         return a^b;
     }
-    //% blockId=obMaths_isPrime block="is prime $x"
-    export function isPrime(x: number): number{
-        if(x == 0) return 0
-        if(x == 1) return 0
-        if(x == 2) return 1
-        if(x % 2 == 0) return 0
-        let numsqrt = Math.sqrt(x);
+    //% blockId=obMaths_isPrime block="is prime $n"
+    export function isPrime(n: number): number{
+        if(n == 0) return 0
+        if(n == 1) return 0
+        if(n == 2) return 1
+        if(n % 2 == 0) return 0
+        let numsqrt = Math.sqrt(n)
         for (let i = 3; i <= numsqrt; i+=2) {
-            if (x % i == 0) return 0
+            if (n % i == 0) return 0
         }
         return 1
     }
-
+    //% blockId=obMaths_plotValue block="plotValue $value $min $max"
+    export function plotValue(value: number,min: number, max:number) {
+        let x = min
+        let dx = (max-min)/25
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 5; j++) {
+                if (x < value) led.plot(j, i)
+                else led.unplot(j, i)
+                x += dx
+            }
+        }
+    }
 }
