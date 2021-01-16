7babab9a7e002751cf0241b17ea6af817b8c805c
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-01-16 14:59:16 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 //% color=#008060 weight=100 icon="\uf12b" block="obMaths";
 namespace obMaths{
+    //%blockId = "obMaths_bor"
+    //%block = "bor $a $b"
+    export function bor(a: number, b: number): number{
+        return a | b;
+    }
+
     //%blockId = "obMaths_xor"
     //%block = "xor $a $b"
     export function xor(a: number, b: number): number{
         return a^b;
     }
     
-    //%blockId = "obMaths_bor"
-    //%block = "bor $a $b"
-    export function bor(a: number, b: number): number{
-        return a | b;
-    }
 }
