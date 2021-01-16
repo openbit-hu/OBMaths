239a333b287c9706cd42cc87c2791993e12f23fd
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-01-16 14:41:10 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,20 +1,19 @@
-//% color=#008060 weight=50 icon="\uf12b" block="obMaths"
 namespace obMaths{
-    //%blockId = obMaths_xor
+    //%blockId = "obMaths_xor"
     //%block = "xor $a $b"
     export function xor(a: number, b: number): number{
         return a^b;
     }
-    //%blockId = obMaths_prime
+    //%blockId = "obMaths_prime"
     //%block = "prime $x"
     export function prime(x: number): boolean{
-        if(x == 0) return false
+/*        if(x == 0) return false
         if(x == 1) return false
         if(x == 2) return true
         let numsqrt = Math.sqrt(x);
         for (let i = 3; i <= numsqrt; i+=2) {
             if (x % i == 0) return false
-        }
+        }*/
         return true
     }
 }
